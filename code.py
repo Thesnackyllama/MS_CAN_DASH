e5bad38c345582452600e785c017e076dc475e1c
Sean Bandley <sean.bandley@uvu.edu>
2023-02-07 15:24:43 -0700

    Changed file names to match ms version

--- code.py
+++ code.py
@@ -17,7 +17,7 @@ def main():
     canbusid = 1512
 
 
-    jsonpath = os.path.abspath(f"C:/Users/10518042/Documents/MS_CAN_DASH/dbc conversion/msdash.json")
+    jsonpath = os.path.abspath(f"C:/Users/10518042/Documents/MS_CAN_DASH/dbc conversion/ms2dash.json")
     with open(jsonpath, 'r') as f:
         db = json.load(f)
 
